c01f5d5bbffa686a6ad331464616e014303961b9
lionel.codingnext <lionel2142014@gmail.com>
2026-07-03 13:37:00 +0700

    a

--- hospital_app.py
+++ hospital_app.py
@@ -147,7 +147,7 @@ with st.form("triage_form"):
     <div style="background:#ccfcff;border:1px solid #5ae7e0;border-radius:14px;
                 padding:20px 24px;margin-bottom:20px;">
         <div style="display:flex;align-items:center;gap:10px;margin-bottom:4px;">
-            <span style="background:#51fae9;color:white;border-radius:8px;
+            <span style="background:#51fae9;color:rgb(0, 64, 254);border-radius:8px;
                          padding:4px 10px;font-size:12px;font-weight:600;">2</span>
             <span style="font-size:16px;font-weight:600;color:#50545e;">How long have you had these symptoms</span>
         </div>
@@ -186,12 +186,12 @@ with st.form("triage_form"):
     # Section 4 — Medical History
     st.markdown("""
     <!-- COLOR5 -->
-    <div style="background:#f60909;border:1px solid #cbdc0d;border-radius:14px;
+    <div style="background:#2340ff;border:1px solid #eff2d1;border-radius:14px;
                 padding:20px 24px;margin-bottom:20px;">
         <div style="display:flex;align-items:center;gap:10px;margin-bottom:4px;">
-            <span style="background:#25f20a;color:white;border-radius:8px;
+            <span style="background:#0af2db;color:rgb(53, 34, 255);border-radius:8px;
                          padding:4px 10px;font-size:12px;font-weight:600;">4</span>
-            <span style="font-size:16px;font-weight:600;color:#105efa;">CODENO20</span>
+            <span style="font-size:16px;font-weight:600;color:#484d58;">Do you have any of the following</span>
         </div>
     </div>
     """, unsafe_allow_html=True)
