4a0b749a3bc93481bd794f61b479464734d53669
lionel.codingnext <lionel2142014@gmail.com>
2026-07-03 13:49:40 +0700

    d

--- hospital_app.py
+++ hospital_app.py
@@ -258,8 +258,8 @@ if submitted:
 
     st.markdown("---")
     st.markdown("""
-    <div style="font-size:22px;font-weight:700;color:#111827;margin-bottom:4px;">w</div>
-    <div style="font-size:14px;color:#6b7280;margin-bottom:1.5rem;">CODENO29</div>
+    <div style="font-size:22px;font-weight:700;color:#111827;margin-bottom:4px;">AI recommendation</div>
+    <div style="font-size:14px;color:#6b7280;margin-bottom:1.5rem;">Base on the information you provided</div>
     """, unsafe_allow_html=True)
 
     res_col, prob_col = st.columns([3, 2])
@@ -287,7 +287,7 @@ if submitted:
             {steps_html}
             <div style="margin-top:20px;padding:12px 16px;background:rgba(0,0,0,0.05);
                         border-radius:10px;font-size:12px;color:#6b7280;line-height:1.5;">
-                ⚠️ CODENO30
+                ⚠️ This isan AI suggestion,not a medicial diagnosis. Please consult a doctor for further evaluation.
             </div>
         </div>
         """, unsafe_allow_html=True)
